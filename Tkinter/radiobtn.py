cfbf9bc691ee9da2702efcbf315ef33b7abc3134
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-07-30 00:35:30 +0530

    nd this is the second example

--- Tkinter/radiobtn.py
+++ Tkinter/radiobtn.py
@@ -24,3 +24,26 @@ R3.pack( anchor = W)
 label = Label(top)  
 label.pack()  
 top.mainloop()  
+
+
+<------------------2nd example-------------------->
+from tkinter import *
+
+def click_me():
+    print(i.get())
+top=Tk(className='RadioButton')
+i= IntVar()
+
+r1 = Radiobutton(top,text="C",value=1,variable=i,font=(None,20))
+r2 = Radiobutton(top,text="C++",value=2,variable=i,font=(None,20))
+r3 = Radiobutton(top,text="java",value=3,variable=i,font=(None,20))
+r1.pack()
+r2.pack()
+r3.pack()
+
+btn = Button(top,text="check",command=click_me)
+
+
+btn.pack()
+top.geometry("300x300+220+120")
+top.mainloop()
