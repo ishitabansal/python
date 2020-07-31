d54f723384d196a8e715b5d4c9a220f9404a4f34
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-07-31 16:10:21 +0530

    add label in the canvas

--- Tkinter/CALCULATOR1.py
+++ Tkinter/CALCULATOR1.py
@@ -13,3 +13,19 @@ canvas1.create_window(210,140, window=entry2)
 
 entry3 = tk.Entry(top)
 canvas1.create_window(210,240, window=entry3)
+
+label = tk.Label(top,text='Calculator')
+label.config(font=('helvetica', 20))
+canvas1.create_window(150,40,window=label)
+
+label1 = tk.Label(top,text="ENTER VALUE 1 : ")
+label1.config(font=('helvetica', 7))
+canvas1.create_window(100,100,window=label1)
+
+label2 = tk.Label(top,text="ENTER VALUE 2 : ")
+label2.config(font=('helvetica', 7))
+canvas1.create_window(100,140,window=label2)
+
+label3 = tk.Label(top,text="RESULT : ")
+label3.config(font=('helvetica', 10))
+canvas1.create_window(100,240,window=label3)
