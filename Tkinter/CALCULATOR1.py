9722b109fdd54bc5cdd6c3fd75f5f1f5eefdbab2
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-07-31 16:11:25 +0530

    adding a sub and add function in  the canvas

--- Tkinter/CALCULATOR1.py
+++ Tkinter/CALCULATOR1.py
@@ -29,3 +29,26 @@ canvas1.create_window(100,140,window=label2)
 label3 = tk.Label(top,text="RESULT : ")
 label3.config(font=('helvetica', 10))
 canvas1.create_window(100,240,window=label3)
+
+
+def add():  
+    v1 = entry1.get()
+    v2 = entry2.get()
+  
+    label4 = tk.Label(top, text= float(v1)+float(v2),font=('helvetica', 10, 'bold'),bg='white')
+    canvas1.create_window(210, 240, window=label4)
+      
+btnAdd = tk.Button(text='+', command=add, bg='light green', fg='black', font=('helvetica', 11, 'bold'), width = 5)
+canvas1.create_window(90, 190, window=btnAdd)
+
+
+
+def sub():  
+    v1 = entry1.get()
+    v2 = entry2.get()
+  
+    label5= tk.Label(top, text= float(v1)-float(v2),font=('helvetica', 10, 'bold'),bg='white')
+    canvas1.create_window(220, 240, window=label5)
+      
+btnSub = tk.Button(text='-', command=sub, bg='violet', fg='black', font=('helvetica', 11, 'bold'), width = 5)
+canvas1.create_window(150, 190, window=btnSub)
