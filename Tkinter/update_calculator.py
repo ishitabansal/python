ca06bd14d47d038ee54b449215718011dac47230
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-08-01 18:18:03 +0530

    create press and press 1 function

--- Tkinter/update_calculator.py
+++ Tkinter/update_calculator.py
@@ -2,3 +2,20 @@ import tkinter as tk
 from tkinter import ttk   #import the tkinter
 
 expr=" "          # create global variable
+
+def press(num):        #create function press
+    global expr
+    expr=expr+str(num)             #converting string
+    eq.set(expr)    #update expression using set method
+
+
+def press1():     #function name equalpress
+    try:
+        global expr      #global variable
+        ttl = str(eval(expr))        #eval function expression evaluate krega and str convert into the string
+        eq.set(ttl)                    #dispplay the total number
+
+        expr = " "                          #show expression and initialize the expression by empty string
+    except:
+        eq.set("ERROR")           #show the error msg
+        expr = " "      
