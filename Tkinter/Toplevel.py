dc5618bbf8296b7b1c085b1b46f824756bb6b93c
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-08-10 23:31:03 +0530

    after comment

--- Tkinter/Toplevel.py
+++ Tkinter/Toplevel.py
@@ -2,13 +2,13 @@ from tkinter import *
   
 root = Tk()  
   
-root.geometry("200x200")  
+root.geometry("200x200")               
   
 def open():  
     top = Toplevel(root)  
     top.mainloop()  
   
-btn = Button(root, text = "open", command = open)  
+btn = Button(root, text = "open", command = open)              #create the button
   
 btn.place(x=75,y=50)  
   
